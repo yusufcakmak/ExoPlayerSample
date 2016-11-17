5ed3decdff208a113dca8b90474c0dadfe2c8eb7
Yusuf <yukocan@gmail.com>
2016-11-17 10:59:59 +0300

    VideoPlayaer
    Now video player can play his, dash or other formats

--- app/src/main/java/com/yusufcakmak/exoplayersample/VideoPlayerActivity.java
+++ app/src/main/java/com/yusufcakmak/exoplayersample/VideoPlayerActivity.java
@@ -1,23 +1,21 @@
 package com.yusufcakmak.exoplayersample;
 
+import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.PlaybackControlView;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -26,55 +24,100 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.TransferListener;
 import com.google.android.exoplayer2.util.Util;
 
-public class VideoPlayerActivity extends AppCompatActivity implements PlaybackControlView.VisibilityListener {
+public class VideoPlayerActivity extends Activity {
 
-    private SimpleExoPlayerView simpleExoPlayerView;
     private Handler mainHandler;
-    private BandwidthMeter bandwidthMeter;
-    private TrackSelector trackSelector;
-    private TrackSelection.Factory videoTrackSelectionFactory;
-    private LoadControl loadControl;
-    private SimpleExoPlayer player;
-    private DataSource.Factory dataSourceFactory;
-    private ExtractorsFactory extractorsFactory;
-    private MediaSource videoSource;
+    private Timeline.Window window;
+    private SimpleExoPlayerView simpleExoPlayerView;
 
-    private String videoUrl = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+    private DataSource.Factory mediaDataSourceFactory;
+    private SimpleExoPlayer player;
+    private DefaultTrackSelector trackSelector;
+
+    private boolean shouldAutoPlay;
+    private int playerWindow;
+    private long playerPosition;
+    private BandwidthMeter bandwidthMeter;
+    private DefaultExtractorsFactory extractorsFactory;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        shouldAutoPlay = true;
+        bandwidthMeter = new DefaultBandwidthMeter();
+        mediaDataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "mediaPlayerSample"), (TransferListener<? super DataSource>) bandwidthMeter);
+        mainHandler = new Handler();
+        window = new Timeline.Window();
         setContentView(R.layout.activity_video_player);
 
-        mainHandler = new Handler();
-        bandwidthMeter = new DefaultBandwidthMeter();
-        videoTrackSelectionFactory = new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
-        trackSelector = new DefaultTrackSelector(mainHandler, videoTrackSelectionFactory);
-        loadControl = new DefaultLoadControl();
+    }
 
-        player = ExoPlayerFactory.newSimpleInstance(getApplicationContext(), trackSelector, loadControl);
+    private void initializePlayer() {
 
         simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.player_view);
-        simpleExoPlayerView.setControllerVisibilityListener(this);
         simpleExoPlayerView.requestFocus();
+
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        trackSelector = new DefaultTrackSelector(mainHandler, videoTrackSelectionFactory);
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, new DefaultLoadControl(),
+                null, false);
         simpleExoPlayerView.setPlayer(player);
+        player.setPlayWhenReady(shouldAutoPlay);
+        MediaSource mediaSource = new HlsMediaSource(Uri.parse("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"),
+                mediaDataSourceFactory, mainHandler, null);
 
+        /*extractorsFactory = new DefaultExtractorsFactory();
+        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"),
+                mediaDataSourceFactory, extractorsFactory, null, null);*/
+        player.prepare(mediaSource);
+    }
 
-        bandwidthMeter = new DefaultBandwidthMeter();
-        dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "mediaPlayerSample"), (TransferListener<? super DataSource>) bandwidthMeter);
-        extractorsFactory = new DefaultExtractorsFactory();
-        videoSource = new ExtractorMediaSource(Uri.parse(videoUrl), dataSourceFactory, extractorsFactory, null, null);
-        player.prepare(videoSource);
+    private void releasePlayer() {
+        if (player != null) {
+            shouldAutoPlay = player.getPlayWhenReady();
+            playerWindow = player.getCurrentWindowIndex();
+            playerPosition = C.TIME_UNSET;
+            Timeline timeline = player.getCurrentTimeline();
+            if (timeline != null && timeline.getWindow(playerWindow, window).isSeekable) {
+                playerPosition = player.getCurrentPosition();
+            }
+            player.release();
+            player = null;
+            trackSelector = null;
+        }
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        player.setPlayWhenReady(false);
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
     }
 
     @Override
-    public void onVisibilityChange(int visibility) {
+    public void onResume() {
+        super.onResume();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
     }
 }
